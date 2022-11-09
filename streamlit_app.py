e28f20514e1706d72c4b93870aa33477043b570c
Jyoti Jagannath Vedpathak <71622370+jyotivedpathak@users.noreply.github.com>
2022-11-09 14:08:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,14 +27,17 @@ streamlit.dataframe(fruits_to_show)
 
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
+if not fruit_choice:
+streamlit.error('Please select a fruit to get information')
+else:
 
 
 #import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
 #Write the data to the screen
-streamlit.text(fruityvice_response.json())# Deleted after implementation
+#streamlit.text(fruityvice_response.json())# Deleted after implementation
 
 
 # write your own comment -what does the next line do?- Normalization 
@@ -53,6 +56,9 @@ my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+except URLError as e:
+  streamlit.error()
 
 #challenge lab
 add_my_fruit = streamlit.text_input('What fruit would you like information about?','jackfruit')
